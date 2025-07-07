61887b1f23e4303a736f50a84de383a6160d6632
mha <mha@voxteneo.com>
2025-07-07 13:01:47 +0700

    Implemented permission

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,44 +2,32 @@ import CompanionCard from '@/components/CompanionCard';
 import CompanionList from '@/components/CompanionList';
 import CTA from '@/components/CTA';
 import { recentSessions } from '@/constants';
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.action';
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react';
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
 
       <section className="home-section">
-        <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#E5D0FF"
-        />
-        <CompanionCard
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Derivatives & Integrals"
-          subject="maths"
-          duration={45}
-          color="#FFDA6E"
-        />
-        <CompanionCard
-          id="789"
-          name="Verba the Vocabulary Builder"
-          topic="English Literature "
-          subject="language"
-          duration={30}
-          color="#BDE7FF"
-        />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
 
       <section className="home-section">
         <CompanionList
           title="Recently completed sessions"
-          companions={recentSessions}
+          companions={recentSessionCompanions}
           classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
